7509ebc554503be6a9f6c42297f29f6f17f5539b
s.mizukami <s-mizukami@tokyoweld.com>
2024-07-18 16:20:40 +0900

    Provider実装。ただ、装置情報セット処理が上手くいっていない

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Card } from '../app/components/Card';
 import { Machine } from '../app/Type'; // Machine型をインポート
 import { HomeSection } from './components/HomeSection';
 
-const Home: React.FC =  async () => {
-  const response = await   fetch("http://127.0.0.1:5000/machines");
+const Home: React.FC = async () => {
+  const response = await fetch("http://127.0.0.1:5000/machines");
   const json = await response.json();
 
   return (
